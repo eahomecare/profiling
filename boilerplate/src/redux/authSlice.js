65367e615ad6e5f5f9fad692d04226557ba8e839
sp241930 <saurabh.pandey@whitecrowdigital.in>
2023-04-16 22:31:04 +0530

    state correction

--- boilerplate/src/redux/authSlice.js
+++ boilerplate/src/redux/authSlice.js
@@ -1,6 +1,16 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+function parseJwt (token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+
+  return JSON.parse(jsonPayload);
+}
+
 const initialState = {
   status: "idle",
   isLoggedIn: false,
@@ -36,6 +46,7 @@ export const getUsers = createAsyncThunk("auth/getUsers", async () => {
   return data;
 });
 
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -57,12 +68,16 @@ export const authSlice = createSlice({
       state.status = "loading";
     },
     [loginUser.fulfilled]: (state, action) => {
-      const { token, name, email, _id } = action.payload.response;
+      const { access_token } = action.payload;
+      const {email,sub} = parseJwt(access_token);
+      const _id = sub
+      const token = access_token
+      
+      
       localStorage.setItem(
         "login",
-        JSON.stringify({ token, email, _id, name, isLoggedIn: true })
+        JSON.stringify({ token, email, _id, isLoggedIn: true })
       );
-      state.user.name = name;
       state.user.email = email;
       state.user._id = _id;
       state.status = "success";
