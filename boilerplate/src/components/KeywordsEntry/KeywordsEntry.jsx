593be702f19afb38683ea5532d80b69a07dc3e66
sp241930 <saurabh.pandey@whitecrowdigital.in>
2023-04-27 15:44:28 +0530

    added nulk api

--- boilerplate/src/components/KeywordsEntry/KeywordsEntry.jsx
+++ boilerplate/src/components/KeywordsEntry/KeywordsEntry.jsx
@@ -1,54 +1,47 @@
-import { MultiSelect, Button } from '@mantine/core';
-import React, { useState,useEffect } from 'react';
-import AddKeywordsModal from './AddKeywordsModal';
+import { MultiSelect, Button } from "@mantine/core";
+import React, { useState, useEffect,useMemo } from "react";
+import AddKeywordsModal from "./AddKeywordsModal";
 import { useDispatch, useSelector } from "react-redux";
-import { getCustomerKeywords } from '../../redux/keywordSlice';
-import _ from 'lodash'
+import { getCustomerKeywords } from "../../redux/keywordSlice";
+import _ from "lodash";
 
-
-const KeywordsEntry = ({props}) => {
-
+const KeywordsEntry = ({ props }) => {
   const [data, setData] = useState([]);
-
 
   const { status, customerDetails } = useSelector((state) => state.customer);
   const dispatch = useDispatch();
 
   const { customerKeywords } = useSelector((state) => state.keyword);
- 
-
-
-
-
 
   const [values, setValue] = useState();
-  const [modalOpened, setModalOpened] = useState(false);
-  const [searchValue, onSearchChange] = useState('');
+  const [searchValue, onSearchChange] = useState("");
+
+  const transformedData = useMemo(
+    () =>
+      _.map(customerKeywords, ({ value }) => ({
+        value: value,
+        label: value,
+      })),
+    [customerKeywords]
+  );
 
 
   useEffect(() => {
 
-    const transformedData = _.map(customerKeywords, ({ value }) => ({ value: value, label: value }));
-    setData(transformedData)
-    setValue(transformedData.map(obj => obj.value))
+    setData(transformedData);
+    setValue(transformedData.map((obj) => obj.value));
 
     console.log(values);
+  }, [customerDetails, dispatch,data]);
 
-  
-},[customerDetails,dispatch])
+  useEffect(() => {
+    dispatch(getCustomerKeywords(customerDetails.id));
+  }, []);
 
 
-useEffect(() => {
-  dispatch(getCustomerKeywords(customerDetails.id));
-}, []);
-
-  function handleModalOpen() {
-    setModalOpened(true);
-  }
-
-  function handleModalClose() {
-    setModalOpened(false);
-  }
+  useEffect(() => {
+    
+  },[])
 
   return (
     <>
@@ -56,19 +49,22 @@ useEffect(() => {
         data={data}
         value={values}
         onChange={(e) => setValue(e)}
-        defaultValue={{ value: 'react', label: 'React' }}
+        defaultValue={{ value: "react", label: "React" }}
         label="Keywords"
         searchable
         searchValue={searchValue}
         onSearchChange={onSearchChange}
         clearable
         placeholder="Pick all that you like"
+        creatable
+        getCreateLabel={(query) => `+ Create ${query}`}
+        onCreate={(query) => {
+          const item = { value: query, label: query };
+          setData((current) => [...current, item]);
+          return item;
+        }}
       />
-      <Button onClick={handleModalOpen}>Add Keywords</Button>
-      <AddKeywordsModal
-        opened={modalOpened}
-        onClose={handleModalClose}
-      />
+
     </>
   );
 };
