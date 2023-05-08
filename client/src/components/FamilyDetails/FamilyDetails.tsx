10bc3a679589f1eefff6aad596ec2a69cda37e7c
Bryce <bfernandes.extern@europ-assistance.in>
2023-05-08 10:58:26 +0530

    First standup push

--- client/src/components/FamilyDetails/FamilyDetails.tsx
+++ client/src/components/FamilyDetails/FamilyDetails.tsx
@@ -1,8 +1,9 @@
-import { EditableTable } from '../common/EditableTable';
+import React from 'react';
+import { EditableTable } from './CustomEditableTable';
 
-const title = 'Family Details'
+const title = 'Family Details';
 
-interface TableSelectionProps {
+export interface TableSelectionProps {
     id: string;
     name: string;
     gender: string;
@@ -22,23 +23,23 @@ const initialData: TableSelectionProps[] = [
     },
     {
         id: '2',
-        name: 'Sushma Sharma',
-        gender: 'Female',
-        relationship: 'Spouse',
-        phone: '+91 834273824',
-        email: 'sushmasharma123@email.com',
+        name: 'Anand Sharma',
+        gender: 'Male',
+        relationship: 'Son',
+        phone: '+91 873624782',
+        email: 'anandsharma123@email.com',
     },
     {
         id: '3',
-        name: 'Sushma Sharma',
-        gender: 'Female',
-        relationship: 'Spouse',
-        phone: '+91 834273824',
-        email: 'sushmasharma123@email.com',
+        name: 'Sarthak Sharma',
+        gender: 'Male',
+        relationship: 'Son',
+        phone: '-',
+        email: '-',
     },
 ];
 
-const headerData = ['Name', 'Gender', 'Relationship', 'Phone No.', 'Email Address'];
+const headerData = ['Name', 'Gender', 'Relationship', 'Phone No.', 'Email Address', '', ''];
 
 const createEmptyRow = (): TableSelectionProps => ({
     id: '',
@@ -50,6 +51,15 @@ const createEmptyRow = (): TableSelectionProps => ({
 
 });
 
+
 export function FamilyDetails() {
-    return <EditableTable<TableSelectionProps> title={title} initialData={initialData} headerData={headerData} createEmptyRow={createEmptyRow} />;
+    return (
+        <EditableTable<TableSelectionProps>
+            title={title}
+            initialData={initialData}
+            headerData={headerData}
+            createEmptyRow={createEmptyRow}
+            rowData={createEmptyRow()}
+        />
+    );
 }
