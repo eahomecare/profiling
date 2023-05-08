82c0eb43d524687f1f37dd2e3648f24113a90444
sp241930 <saurabh.pandey@whitecrowdigital.in>
2023-05-08 23:20:57 +0530

    table

--- boilerplate/src/components/Occupation/Occupation.jsx
+++ boilerplate/src/components/Occupation/Occupation.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import EditableTable from '../EditableTable/EditableTable'
+import { EditableTable } from '../EditableTable/EditableTable';
 
-const Occupation = () => {
-  return (
-   <><EditableTable/></>
-  )
+const title = 'Occupation'
+
+
+
+const initialData = [
+    {
+        id: '1',
+        type: 'Property Dealer',
+        industry: 'Business',
+        to: '2005',
+        from: '2009',
+        incomeBracket: '₹7,00,000 - ₹15,00,000',
+    },
+    {
+        id: '2',
+        type: 'Stock Exchange',
+        industry: 'Finance',
+        to: '2008',
+        from: 'Present',
+        incomeBracket: '₹15,00,000 - ₹30,00,000',
+    },
+];
+
+const headerData = ['Type', 'Industry', 'To', 'From', 'Income Bracket'];
+
+const createEmptyRow = () => ({
+    id: '',
+    type: '',
+    industry: '',
+    to: '',
+    from: '',
+    incomeBracket: '',
+});
+
+export function Occupation() {
+    return <EditableTable title={title} initialData={initialData} headerData={headerData} createEmptyRow={createEmptyRow} />;
 }
-
-export default Occupation
