afd301a4bdbfafd4663b9e55c66e21a4b1d1a1b7
sp241930 <saurabh.pandey@whitecrowdigital.in>
2023-05-31 14:36:24 +0530

    profile percentrage

---
+++ server/src/profile/profile.controller.ts
@@ -0,0 +1,55 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Controller('profile')
+export class ProfileController {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  @Get(':customerId')
+  async getProfileCompletion(@Param('customerId') customerId: string) {
+    try {
+      const customer = await this.prisma.customer.findUnique({
+        where: { id: customerId },
+        include: {
+          personal_details: true,
+          keywords: true,
+          OccupationCustomerMappings: true,
+          VehicleCustomerMappings: true,
+        },
+      });
+
+
+      console.log(customer);
+      
+
+      let completionPercentage = 0;
+
+      if (customer.personal_details) {
+        completionPercentage += 25;
+      }
+
+      if (customer.OccupationCustomerMappings.length > 0) {
+        completionPercentage += 25;
+      }
+
+      if (customer.VehicleCustomerMappings.length > 0) {
+        completionPercentage += 25;
+      }
+
+      if (customer.keywords.length >= 10) {
+        completionPercentage += 25;
+      } else if (customer.keywords.length >= 5) {
+        completionPercentage += 12.5;
+      }
+
+      return { completionPercentage };
+    } catch (error) {
+    } finally {
+      await this.prisma.$disconnect();
+    }
+  }
+}
