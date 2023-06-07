48f5bd17ef70891bcd7dea6e0d8c8945c3b31d6a
sp241930 <saurabh.pandey@whitecrowdigital.in>
2023-06-07 23:01:38 +0530

    model changes

--- server/src/customer-question-history/customer-question-history.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CustomerQuestionHistoryController } from './customer-question-history.controller';
-import { CustomerQuestionHistoryService } from './customer-question-history.service';
-
-@Module({
-  controllers: [CustomerQuestionHistoryController],
-  providers: [CustomerQuestionHistoryService]
-})
-export class CustomerQuestionHistoryModule {}
