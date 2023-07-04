6bd242b2024737463d81f848d92292c62377c5ab
saurabh241930 <sp241930@gmail.com>
2023-07-04 20:15:47 +0530

    cors issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: ['https://custprofile.europ-assistance.in', 'http://localhost:5000'],
     methods: 'GET,POST,PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   })
