f590cced19bda28d1b03bc40282f3f53ec8c82de
saurabh241930 <sp241930@gmail.com>
2023-07-04 20:20:31 +0530

    hot fix / cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://custprofile.europ-assistance.in', 'http://localhost:5000'],
-    methods: 'GET,POST,PATCH',
-    allowedHeaders: 'Content-Type, Authorization',
-  })
+  // app.enableCors({
+  //   origin: ['https://custprofile.europ-assistance.in', 'http://localhost:5000'],
+  //   methods: 'GET,POST,PATCH',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // })
+
+  app.enableCors()
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
