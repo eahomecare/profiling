dbfc2947dedfa18af133d4877aa4a1ef93e62eb7
Bryce <bfernandes.extern@europ-assistance.in>
2023-07-10 17:57:01 +0530

    alternate cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     // origin: ['https://custprofile.europ-assistance.in', 'http://localhost:5000'],
     origin: ['*'],
     methods: 'GET,POST,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true
+    // credentials: true,
+    preflightContinue: true
   })
   app.useGlobalPipes(
     new ValidationPipe({
