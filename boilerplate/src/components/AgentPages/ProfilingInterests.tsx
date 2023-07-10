b0eb98fe9d827df30f9603d1084fbead9c410cbc
Bryce <bfernandes.extern@europ-assistance.in>
2023-07-10 07:26:06 +0530

    Agent pages and atlas db dump added

---
+++ boilerplate/src/components/AgentPages/ProfilingInterests.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Group, Text, Avatar, Center } from '@mantine/core';
+import assets from '../Profiling/assets';
+
+type AssetKeys = keyof typeof assets;
+
+interface ProfilingInterestsProps {
+    profilingInterests: AssetKeys[];
+}
+
+const ProfilingInterests: React.FC<ProfilingInterestsProps> = ({ profilingInterests }) => {
+    const ICON_SIZE = 80;
+
+    return (
+        <Group position="center">
+            {profilingInterests.map((interest) => {
+                if (interest) {
+                    const url = assets[interest];
+
+                    return (
+                        <div key={interest}>
+                            <Avatar
+                                size={ICON_SIZE}
+                                radius={ICON_SIZE}
+                                src={url}
+                            />
+                            <Center>
+                                <Text fw={550} size={15}>{interest}</Text>
+                            </Center>
+                        </div>
+                    );
+                }
+            })}
+        </Group>
+    );
+};
+
+export default ProfilingInterests;
