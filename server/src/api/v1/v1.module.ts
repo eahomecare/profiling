f4b0f018159c00d5d3fc403cc22b9d48220ed6da
Bryce <bfernandes.extern@europ-assistance.in>
2023-07-28 07:36:10 +0530

    login route working

--- server/src/api/v1/v1.module.ts
+++ server/src/api/v1/v1.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { V1Controller } from './v1.controller';
 import { V1Service } from './v1.service';
+import { AgentSessionService } from './agent-session.service';
 
 @Module({
   controllers: [V1Controller],
-  providers: [V1Service]
+  providers: [V1Service, AgentSessionService]
 })
-export class V1Module {}
+export class V1Module { }
