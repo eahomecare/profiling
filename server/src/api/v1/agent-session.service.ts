6595194144dcd5dcc0d7c3cc4be6df2e381fcfc8
Bryce <bfernandes.extern@europ-assistance.in>
2023-07-28 07:36:10 +0530

    login route working

---
+++ server/src/api/v1/agent-session.service.ts
@@ -0,0 +1,69 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { randomBytes } from 'crypto';
+import { ConfigService } from '@nestjs/config';
+import { CreateAgentSessionDto } from './dto/agent-session.dto';
+import { sign } from 'jsonwebtoken';
+
+@Injectable()
+export class AgentSessionService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService,
+    ) { }
+
+    async create(
+        createAgentSessionDto: CreateAgentSessionDto,
+        crmName: string,
+    ) {
+        const user = await this.prisma.user.findUnique({
+            where: { agentJWT: createAgentSessionDto.agentAuthenticationKey },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid agent authentication key');
+        }
+
+        const agentSessions = await this.prisma.agentSession.findMany({
+            where: { CRM: crmName },
+        });
+
+        for (const agentSession of agentSessions) {
+            const existingSession = await this.prisma.userAgentSessionMapping.findUnique({
+                where: { userId_sessionId: { userId: user.id, sessionId: agentSession.id } },
+            });
+
+            if (existingSession) {
+                await this.prisma.userAgentSessionMapping.delete({
+                    where: { id: existingSession.id },
+                });
+
+                await this.prisma.agentSession.delete({
+                    where: { id: existingSession.sessionId },
+                });
+
+                throw new UnauthorizedException('Existing session found and destroyed');
+            }
+        }
+
+        const randomValue = randomBytes(16).toString('hex');
+        const secret = this.configService.get('JWT_SECRET');
+
+        const authorizationToken = sign(
+            { agentAuthenticationKey: createAgentSessionDto.agentAuthenticationKey, randomValue },
+            secret,
+        );
+
+        await this.prisma.agentSession.create({
+            data: {
+                CRM: crmName,
+                authorizationToken,
+                userAgentSessionMapping: {
+                    create: { user: { connect: { id: user.id } } },
+                },
+            },
+        });
+
+        return { agentAuthorizationToken: authorizationToken };
+    }
+}
--- server/src/api/v1/v1.controller.ts
+++ server/src/api/v1/v1.controller.ts
@@ -6,15 +6,25 @@ import {
     Post,
     Res,
     Req,
-    ValidationPipe
+    Headers,
+    ValidationPipe,
+    UnauthorizedException,
+    UsePipes
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { V1Service } from './v1.service';
 import { RegisterAgentDto } from './dto/register-agent.dto';
+import { ConfigService } from '@nestjs/config';
+import { CreateAgentSessionDto } from './dto/agent-session.dto';
+import { AgentSessionService } from './agent-session.service';
 
 @Controller('api/v1')
 export class V1Controller {
-    constructor(private readonly v1Service: V1Service) { }
+    constructor(
+        private readonly v1Service: V1Service,
+        private readonly agentSessionService: AgentSessionService,
+        private readonly configService: ConfigService,
+    ) { }
 
     @Post('/register')
     async registerAgent(
@@ -37,6 +47,52 @@ export class V1Controller {
             const msg = await this.v1Service.registerAgent(data);
 
             res.status(HttpStatus.CREATED).json({ ...msg, success: true });
+        } catch (error) {
+            let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
+            let message = 'Internal server error';
+
+            if (error instanceof HttpException) {
+                statusCode = error.getStatus();
+                message = error.message;
+            }
+
+            res.status(statusCode).json({
+                success: false,
+                status: statusCode,
+                message: message
+            });
+        }
+    }
+
+    @Post('/login')
+    async loginAgent(
+        @Req() request: Request,
+        @Body(new ValidationPipe({ exceptionFactory: (errors) => new HttpException(errors, HttpStatus.BAD_REQUEST) })) createAgentSessionDto: CreateAgentSessionDto,
+        @Res() res: Response
+    ) {
+        try {
+            const authorizationHeader = request.headers.authorization;
+            if (!authorizationHeader)
+                throw new HttpException('No Authorization Header Present', HttpStatus.UNAUTHORIZED)
+
+            const encodedStaticKey = authorizationHeader.split(' ')[1];
+            if (!encodedStaticKey)
+                throw new HttpException('Invalid Authorization Format', HttpStatus.UNAUTHORIZED)
+
+            const staticKey = Buffer.from(encodedStaticKey, 'base64').toString();
+
+            const crmNames = ['HC', 'CRM2', 'CRM3']; // update this with the actual CRM names
+            const crmName = crmNames.find(
+                (crm) => this.configService.get(crm + '_STATIC_KEY') === staticKey
+            );
+
+            if (!crmName) {
+                throw new UnauthorizedException('Invalid Static Key');
+            }
+
+            const result = await this.agentSessionService.create(createAgentSessionDto, crmName);
+
+            res.status(HttpStatus.OK).json({ success: true, ...result });
         } catch (error) {
             let statusCode = HttpStatus.INTERNAL_SERVER_ERROR;
             let message = 'Internal server error';
