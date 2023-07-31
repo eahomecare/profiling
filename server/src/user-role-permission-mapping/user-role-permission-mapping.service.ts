363babadbc75377b9017ca83f63430efb038a87a
saurabh241930 <sp241930@gmail.com>
2023-07-31 22:05:53 +0530

    mapping changes

--- server/src/user-role-permission-mapping/user-role-permission-mapping.service.ts
+++ server/src/user-role-permission-mapping/user-role-permission-mapping.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, userRolePermissionMapping } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserRolePermissionMappingService {}
+export class UserRolePermissionMappingService {
+    constructor(private prisma: PrismaService) { }
+
+    async create(data: Prisma.userRolePermissionMappingCreateInput): Promise<userRolePermissionMapping> {
+        try {
+            const createdMapping = await this.prisma.userRolePermissionMapping.create({
+                data,
+            });
+            return createdMapping;
+        } catch (error) {
+            throw new Error('Failed to create userRolePermissionMapping');
+        }
+    }
+
+    async findAll(): Promise<userRolePermissionMapping[]> {
+        try {
+            const mappings = await this.prisma.userRolePermissionMapping.findMany();
+            return mappings;
+        } catch (error) {
+            throw new Error('Failed to fetch userRolePermissionMappings');
+        }
+    }
+
+    async findMappingsByUserId(userId: string): Promise<userRolePermissionMapping[]> {
+        try {
+            const mappings = await this.prisma.userRolePermissionMapping.findMany({
+                where: {
+                    userId,
+                },
+            });
+            return mappings;
+        } catch (error) {
+            throw new Error('Failed to fetch userRolePermissionMappings by userId');
+        }
+    }
+
+    async findMappingsByRoleId(roleId: string): Promise<userRolePermissionMapping[]> {
+        try {
+            const mappings = await this.prisma.userRolePermissionMapping.findMany({
+                where: {
+                    roleId,
+                },
+            });
+            return mappings;
+        } catch (error) {
+            throw new Error('Failed to fetch userRolePermissionMappings by roleId');
+        }
+    }
+}
