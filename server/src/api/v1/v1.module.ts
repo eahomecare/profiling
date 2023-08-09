9a701d0e93f40b7b94b2b907b499a54ccde996d2
Bryce <bfernandes.extern@europ-assistance.in>
2023-08-09 07:55:43 +0530

    v1/keywords completed

--- server/src/api/v1/v1.module.ts
+++ server/src/api/v1/v1.module.ts
@@ -7,10 +7,11 @@ import { PrismaService } from '../../prisma/prisma.service';
 import { AuthorizationService } from './authorization.service';
 import { LogoutAgentService } from './logoutAgent.service';
 import { ValidateAgentTokenService } from './validateAgentToken.service';
+import { KeywordsService } from './agentKeyword.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [V1Controller],
-  providers: [RegisterAgentService, CreateAgentSessionService, PrismaService, AuthorizationService, LogoutAgentService, ValidateAgentTokenService],
+  providers: [RegisterAgentService, CreateAgentSessionService, PrismaService, AuthorizationService, LogoutAgentService, ValidateAgentTokenService, KeywordsService],
 })
 export class V1Module { }
