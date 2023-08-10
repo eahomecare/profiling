71f6f674f5293469fd3e0404a919b28aac1a83d1
Bryce <bfernandes.extern@europ-assistance.in>
2023-08-10 21:24:47 +0530

    Finished agent submit

--- server/src/api/v1/agentSubmit.service.ts
+++ server/src/api/v1/agentSubmit.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, PreconditionFailedException, UnprocessableEntityException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UserAgentSessionMapping } from '@prisma/client';
 
@@ -8,122 +8,158 @@ export class SubmitService {
 
     async connectCustomerToKeywords(customerId: string, keywordIds: string[]): Promise<any[]> {
         let connectedKeywords = [];
-        for (const keywordId of keywordIds) {
-            const updatedKeyword = await this.prisma.keyword.update({
-                where: { id: keywordId },
-                data: { customers: { connect: { id: customerId } } }
-            });
-            connectedKeywords.push(updatedKeyword);
+        try {
+            for (const keywordId of keywordIds) {
+                const updatedKeyword = await this.prisma.keyword.update({
+                    where: { id: keywordId },
+                    data: { customers: { connect: { id: customerId } } }
+                });
+                connectedKeywords.push(updatedKeyword);
+            }
+            return connectedKeywords;
+        } catch (error) {
+            console.error("Error in connectCustomerToKeywords:", error);
+            throw new PreconditionFailedException('Keywords and Customers seem to not exist. Check and try again')
         }
-        return connectedKeywords;
-    }
-
-    async handleRemarks(customerId: string, remark: string): Promise<void> {
-        // Assuming remarks are stored elsewhere as well, you can fill this function as needed
     }
 
     async handleCreatedKeywords(customerId: string, agentId: string, keywords: string[]): Promise<string[]> {
         let createdKeywordIds = [];
-
-        for (const keyword of keywords) {
-            const createdKeyword = await this.prisma.createdKeywords.create({
-                data: {
-                    keyword: keyword,
-                    customerID: customerId,
-                    agentID: agentId,
-                    submitID: null
-                }
-            });
-            createdKeywordIds.push(createdKeyword.id);
+        try {
+            for (const keyword of keywords) {
+                const createdKeyword = await this.prisma.createdKeywords.create({
+                    data: {
+                        keyword: keyword,
+                        customerID: customerId,
+                        agentID: agentId,
+                        submitID: '64773d52c2f9d843b9393939'
+                    }
+                });
+                createdKeywordIds.push(createdKeyword.id);
+            }
+            return createdKeywordIds;
+        } catch (error) {
+            console.error("Error in handleCreatedKeywords:", error);
+            throw new UnprocessableEntityException('createdKeywords is not structured properly, check and try again.')
         }
-
-        return createdKeywordIds;
     }
 
     async handleQuestionResponses(questionResponses: any[], customerId: string): Promise<any> {
         let createdQuestions = [];
         let createdKeywordIds = [];
-        for (const item of questionResponses) {
-            const {
-                question,
-                category,
-                level,
-                type,
-                options,
-                selectedOptions
-            } = item;
-
-            const createdQuestion = await this.prisma.question.create({
-                data: {
+        try {
+            for (const item of questionResponses) {
+                const {
                     question,
                     category,
                     level,
                     type,
                     options,
-                    customers: { connect: { id: customerId } },
-                },
-            });
-            createdQuestions.push(createdQuestion.id);
+                    selectedOptions
+                } = item;
 
-            for (const option of options) {
-                if (selectedOptions.includes(option)) {
-                    const createdKeyword = await this.prisma.keyword.create({
-                        data: {
-                            category,
-                            value: option,
-                            level,
-                            customers: { connect: { id: customerId } },
-                            questionIDs: [createdQuestion.id],
-                        },
-                    });
-                    createdKeywordIds.push(createdKeyword.id);
+                const createdQuestion = await this.prisma.question.create({
+                    data: {
+                        question,
+                        category,
+                        level,
+                        type,
+                        options,
+                        customers: { connect: { id: customerId } },
+                    },
+                });
+                createdQuestions.push(createdQuestion.id);
 
-                    await this.prisma.customer.update({
-                        where: { id: customerId },
-                        data: {
-                            keywordIDs: { push: createdKeyword.id },
-                        },
-                    });
+                for (const option of options) {
+                    if (selectedOptions.includes(option)) {
+                        const createdKeyword = await this.prisma.keyword.create({
+                            data: {
+                                category,
+                                value: option,
+                                level,
+                                customers: { connect: { id: customerId } },
+                                questionIDs: [createdQuestion.id],
+                            },
+                        });
+                        createdKeywordIds.push(createdKeyword.id);
+
+                        await this.prisma.customer.update({
+                            where: { id: customerId },
+                            data: {
+                                keywordIDs: { push: createdKeyword.id },
+                            },
+                        });
+                    }
                 }
             }
+            return {
+                questions: createdQuestions,
+                keywords: createdKeywordIds
+            };
+        } catch (error) {
+            console.error("Error in handleQuestionResponses:", error);
+            throw new UnprocessableEntityException('questionResponses is not structured properly, check and try again.')
         }
-        return {
-            questions: createdQuestions,
-            keywords: createdKeywordIds
-        };
     }
 
     async findCustomerByMobile(mobile: string): Promise<any> {
-        return this.prisma.customer.findUnique({
-            where: { mobile: mobile }
-        });
+        try {
+            return this.prisma.customer.findUnique({
+                where: { mobile: mobile }
+            });
+        } catch (error) {
+            console.error("Error in findCustomerByMobile:", error);
+            throw new NotFoundException('Mobile provided not found')
+        }
     }
 
     async findUserByAgentSession(sessionID: string): Promise<UserAgentSessionMapping> {
-        const userAgentMapping = await this.prisma.userAgentSessionMapping.findFirst({
-            where: { sessionId: sessionID }
-        });
-        return userAgentMapping;
+        try {
+            const userAgentMapping = await this.prisma.userAgentSessionMapping.findFirst({
+                where: { sessionId: sessionID }
+            });
+            return userAgentMapping;
+        } catch (error) {
+            console.error("Error in findUserByAgentSession:", error);
+            throw new NotFoundException('Agent session not found')
+        }
     }
 
     async getAgentIDFromSession(agentSession: UserAgentSessionMapping): Promise<any> {
-        const user = await this.prisma.user.findUnique({
-            where: { id: agentSession.userId }
-        })
-        return user.agentID
+        try {
+            const user = await this.prisma.user.findUnique({
+                where: { id: agentSession.userId }
+            })
+            return user.agentID
+        } catch (error) {
+            console.error("Error in getAgentIDFromSession:", error);
+            throw new NotFoundException('Agent ID missing')
+        }
     }
 
-    async createAgentSubmit(data: any, createdKeywordIds: string[]): Promise<void> {
-        const createdAgentSubmit = await this.prisma.agentSubmits.create({
-            data: data
-        });
+    async createAgentSubmit(data: any,): Promise<void> {
+        try {
+            const { createdKeywords, questions, Keywords, ...otherData } = data;
 
-        // Update the createdKeywords with the submitID
-        for (const keywordId of createdKeywordIds) {
-            await this.prisma.createdKeywords.update({
-                where: { id: keywordId },
-                data: { submitID: createdAgentSubmit.id }
+            const createdAgentSubmit = await this.prisma.agentSubmits.create({
+                data: {
+                    ...otherData,
+                    createdKeywords: { connect: createdKeywords.map(id => ({ id })) },
+                    questions: { connect: questions.map(id => ({ id })) },
+                    Keywords: { connect: Keywords.map(id => ({ id })) },
+                }
             });
+
+            // Update the createdKeywords with the submitID
+            for (const keywordId of data.createdKeywords) {
+                await this.prisma.createdKeywords.update({
+                    where: { id: keywordId },
+                    data: { submitID: createdAgentSubmit.id }
+                });
+            }
+        } catch (error) {
+            console.error("Error in createAgentSubmit:", error);
+            throw new UnprocessableEntityException('Something went wrong in processing the data. Please check the request and try again')
         }
     }
 }
