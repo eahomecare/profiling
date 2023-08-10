64d9410623559cd75a91c226dca95b7dfebd9d1b
Bryce <bfernandes.extern@europ-assistance.in>
2023-08-10 18:43:57 +0530

    Fetch questions completed, working on submit testing

--- server/src/api/v1/v1.module.ts
+++ server/src/api/v1/v1.module.ts
@@ -8,12 +8,14 @@ import { AuthorizationService } from './authorization.service';
 import { LogoutAgentService } from './logoutAgent.service';
 import { ValidateAgentTokenService } from './validateAgentToken.service';
 import { KeywordsService } from './agentKeyword.service';
-import { SearchService } from 'src/search/search.service';
 import { SearchModule } from 'src/search/search.module';
+import { AgentQuestionService } from './agentQuestion.service';
+import { LangchainService } from 'src/langchain/langchain.service';
+import { SubmitService } from './agentSubmit.service';
 
 @Module({
   imports: [ConfigModule, SearchModule],
   controllers: [V1Controller],
-  providers: [RegisterAgentService, CreateAgentSessionService, PrismaService, AuthorizationService, LogoutAgentService, ValidateAgentTokenService, KeywordsService],
+  providers: [RegisterAgentService, CreateAgentSessionService, PrismaService, AuthorizationService, LogoutAgentService, ValidateAgentTokenService, KeywordsService, AgentQuestionService, LangchainService, SubmitService],
 })
 export class V1Module { }
