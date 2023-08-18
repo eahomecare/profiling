125ab7dc52c6764516a246e2c4b66ead5c1cb8d5
saurabh241930 <sp241930@gmail.com>
2023-08-18 17:34:58 +0530

    few more acl changes

--- server/src/user-role-permission-mapping/user-role-permission-mapping.service.ts
+++ server/src/user-role-permission-mapping/user-role-permission-mapping.service.ts
@@ -1,71 +1,110 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, userRolePermissionMapping } from '@prisma/client';
+import {
+  Prisma,
+  userRolePermissionMapping,
+} from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserRolePermissionMappingService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async create(data: Prisma.userRolePermissionMappingCreateInput): Promise<userRolePermissionMapping> {
-        try {
-            const createdMapping = await this.prisma.userRolePermissionMapping.create({
-                data,
-            });
+  async create(
+    data: Prisma.userRolePermissionMappingCreateInput,
+  ): Promise<userRolePermissionMapping> {
+    try {
+      const createdMapping =
+        await this.prisma.userRolePermissionMapping.create(
+          {
+            data,
+          },
+        );
 
-            const foundMapping = await this.prisma.userRolePermissionMapping.findUnique({
-                where: { id: createdMapping.id },
-                include: {
-                    role: true,
-                    user: true,
-                    permission: true,
-                },
-            });
+      const foundMapping =
+        await this.prisma.userRolePermissionMapping.findUnique(
+          {
+            where: { id: createdMapping.id },
+            include: {
+              role: true,
+              user: true,
+              permission: true,
+            },
+          },
+        );
 
-            return foundMapping;
-        } catch (error) {
-            throw new Error('Failed to create userRolePermissionMapping');
-        }
+      return foundMapping;
+    } catch (error) {
+      throw new Error(
+        'Failed to create userRolePermissionMapping',
+      );
     }
+  }
 
-
-    async findAll(): Promise<userRolePermissionMapping[]> {
-        try {
-            const mappings = await this.prisma.userRolePermissionMapping.findMany({
-                include: {
-                    role: true,
-                    user: true,
-                    permission: true,
-                },
-            });
-            return mappings;
-        } catch (error) {
-            throw new Error('Failed to fetch userRolePermissionMappings');
-        }
+  async findAll(): Promise<
+    userRolePermissionMapping[]
+  > {
+    try {
+      const mappings =
+        await this.prisma.userRolePermissionMapping.findMany(
+          {
+            include: {
+              role: true,
+              user: true,
+              permission: true,
+            },
+          },
+        );
+      return mappings;
+    } catch (error) {
+      throw new Error(
+        'Failed to fetch userRolePermissionMappings',
+      );
     }
+  }
 
-    async findMappingsByUserId(userId: string): Promise<userRolePermissionMapping[]> {
-        try {
-            const mappings = await this.prisma.userRolePermissionMapping.findMany({
-                where: {
-                    userId,
-                },
-            });
-            return mappings;
-        } catch (error) {
-            throw new Error('Failed to fetch userRolePermissionMappings by userId');
-        }
-    }
+  async findMappingsByUserId(
+    userId: string,
+  ): Promise<userRolePermissionMapping[]> {
+    try {
+      const mappings =
+        await this.prisma.userRolePermissionMapping.findMany(
+          {
+            where: {
+              userId,
+            },
+            include: {
+              role: true,
+              permission: true,
+            },
+          },
+        );
+      return mappings;
+    } catch (error) {
+      console.log(error);
 
-    async findMappingsByRoleId(roleId: string): Promise<userRolePermissionMapping[]> {
-        try {
-            const mappings = await this.prisma.userRolePermissionMapping.findMany({
-                where: {
-                    roleId,
-                },
-            });
-            return mappings;
-        } catch (error) {
-            throw new Error('Failed to fetch userRolePermissionMappings by roleId');
-        }
+      throw new Error(
+        'Failed to fetch userRolePermissionMappings by userId',
+      );
     }
+  }
+
+  async findMappingsByRoleId(
+    roleId: string,
+  ): Promise<userRolePermissionMapping[]> {
+    try {
+      const mappings =
+        await this.prisma.userRolePermissionMapping.findMany(
+          {
+            where: {
+              roleId,
+            },
+          },
+        );
+      return mappings;
+    } catch (error) {
+      throw new Error(
+        'Failed to fetch userRolePermissionMappings by roleId',
+      );
+    }
+  }
 }
