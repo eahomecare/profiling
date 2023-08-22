07c2e6e16cf76eb0c29df7c2a04d7d3620f0b16b
saurabh241930 <sp241930@gmail.com>
2023-08-22 16:07:02 +0530

    types fixes

--- server/src/campaign/campaign.controller.ts
+++ server/src/campaign/campaign.controller.ts
@@ -1,35 +1,39 @@
 // src/campaign/campaign.controller.ts
 
-import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { CampaignService } from './campaign.service';
-import { Campaign } from '@prisma/client';
+import { Campaign, Prisma } from '@prisma/client';
 
 @Controller('campaign')
 export class CampaignController {
-    constructor(private readonly campaignService: CampaignService) { }
+  constructor(
+    private readonly campaignService: CampaignService,
+  ) {}
 
-    @Get()
-    async findAll(): Promise<Campaign[]> {
-        return this.campaignService.findAll();
-    }
+  @Get()
+  async getAllCampaign(): Promise<Campaign[]> {
+    return this.campaignService.findAll();
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: number): Promise<Campaign | null> {
-        return this.campaignService.findOne(id);
-    }
+  @Get(':id')
+  async getCampaignById(
+    @Param('id') id: string,
+  ): Promise<Campaign> {
+    return this.campaignService.findOne(id);
+  }
 
-    @Post()
-    async create(@Body() data: Partial<Campaign>): Promise<Campaign> {
-        return this.campaignService.create(data);
-    }
-
-    @Put(':id')
-    async update(@Param('id') id: number, @Body() data: Partial<Campaign>): Promise<Campaign | null> {
-        return this.campaignService.update(id, data);
-    }
-
-    @Delete(':id')
-    async remove(@Param('id') id: number): Promise<void> {
-        return this.campaignService.remove(id);
-    }
+  @Post()
+  async create(
+    @Body() data: Prisma.CampaignCreateInput,
+  ): Promise<Campaign> {
+    return this.campaignService.create(data);
+  }
 }
--- server/src/campaign/campaign.service.ts
+++ server/src/campaign/campaign.service.ts
@@ -1,39 +1,38 @@
-// src/campaign/campaign.service.ts
-
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Campaign } from '@prisma/client';
+import { Campaign, Prisma } from '@prisma/client';
 
 @Injectable()
 export class CampaignService {
-    constructor(private readonly prisma: PrismaService) { }
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {}
 
-    async findAll(): Promise<Campaign[]> {
-        return this.prisma.campaign.findMany();
-    }
+  async findAll(): Promise<Campaign[]> {
+    return this.prisma.campaign.findMany();
+  }
 
-    async findOne(id: number): Promise<Campaign | null> {
-        return this.prisma.campaign.findUnique({
-            where: { id },
+  async findOne(
+    id: string,
+  ): Promise<Campaign | null> {
+    return this.prisma.campaign.findUnique({
+      where: { id },
+    });
+  }
+
+  async create(
+    data: Prisma.CampaignCreateInput,
+  ): Promise<Campaign> {
+    try {
+      const createdCampaign =
+        await this.prisma.campaign.create({
+          data,
         });
+      return createdCampaign;
+    } catch (error) {
+      throw new Error(
+        'Failed to create permission',
+      );
     }
-
-    async create(data: Partial<Campaign>): Promise<Campaign> {
-        return this.prisma.campaign.create({
-            data,
-        });
-    }
-
-    async update(id: number, data: Partial<Campaign>): Promise<Campaign | null> {
-        return this.prisma.campaign.update({
-            where: { id },
-            data,
-        });
-    }
-
-    async remove(id: number): Promise<void> {
-        await this.prisma.campaign.delete({
-            where: { id },
-        });
-    }
+  }
 }
