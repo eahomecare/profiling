bfbd6421067e090fa7283f83e853ec9a4566fb6b
saurabh241930 <sp241930@gmail.com>
2023-09-12 22:50:54 +0530

    changes

--- boilerplate/src/pages/Acl/_mainLink.js
+++ boilerplate/src/pages/Acl/_mainLink.js
@@ -5,45 +5,55 @@ import {
     IconMessages,
     IconDatabase,
 } from '@tabler/icons-react';
-import { ThemeIcon, UnstyledButton, Group, Text } from '@mantine/core';
+import { IconUsersGroup, IconSettings, IconSquare } from '@tabler/icons-react';
+import { ThemeIcon, UnstyledButton, Group, Text, Button } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
-function MainLink({ icon, color, label }) {
+
+function MainLink({ icon, color, label, link }) {
     return (
-        <UnstyledButton
-            sx={(theme) => ({
+
+        <NavLink to={link}>
+            <UnstyledButton sx={(theme) => ({
                 display: 'block',
                 width: '100%',
                 padding: theme.spacing.xs,
                 borderRadius: theme.radius.sm,
-                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
 
                 '&:hover': {
                     backgroundColor:
                         theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
                 },
-            })}
-        >
-            <Group>
-                <ThemeIcon color={color} variant="light">
-                    {icon}
-                </ThemeIcon>
 
-                <Text size="sm">{label}</Text>
-            </Group>
-        </UnstyledButton>
+                '&:active': {
+                    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                },
+            })}>
+
+                <Group>
+                    <ThemeIcon color={color} variant="light">
+                        {icon}
+                    </ThemeIcon>
+
+                    <Text size="sm">{label}</Text>
+                </Group>
+
+            </UnstyledButton>
+
+        </NavLink>
     );
 }
 
 const data = [
-    { icon: <IconGitPullRequest size="1rem" />, color: 'blue', label: 'Pull Requests' },
-    { icon: <IconAlertCircle size="1rem" />, color: 'teal', label: 'Open Issues' },
-    { icon: <IconMessages size="1rem" />, color: 'violet', label: 'Discussions' },
-    { icon: <IconDatabase size="1rem" />, color: 'grape', label: 'Databases' },
+    { icon: <IconUsersGroup size="1rem" />, color: 'blue', label: 'Users', link: "users" },
+    { icon: <IconDatabase size="1rem" />, color: 'grape', label: 'Roles vs Permission', link: "" },
+    { icon: <IconDatabase size="1rem" />, color: 'green', label: 'Permission vs Users', link: "permissionrolemappings" },
 ];
 
-export function MainLinks() {
-    const links = data.map((link) => <MainLink {...link} key={link.label} />);
+export function MainLinks({ onLinkSelect }) {
+    const links = data.map((link) => <MainLink onClick={() => (onLinkSelect(link))} {...link} key={link.label} />);
     return <div>{links}</div>;
 }
