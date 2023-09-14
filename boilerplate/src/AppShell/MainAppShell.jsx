d97a08b1e22a6e6ba3641568d7d21a83b3d08a50
saurabh241930 <sp241930@gmail.com>
2023-09-14 16:27:10 +0530

    navbar

--- boilerplate/src/AppShell/MainAppShell.jsx
+++ boilerplate/src/AppShell/MainAppShell.jsx
@@ -1,15 +1,25 @@
-import { ActionIcon, Avatar, Center, Container, Flex, Group, Header, LoadingOverlay, Navbar, Space, Stack, Text, TextInput, Title } from "@mantine/core"
-import { Icon3dCubeSphere, IconAccessible, IconAdjustmentsHorizontal, IconAnalyze, IconArrowAutofitUp, IconArrowBadgeDown, IconArrowBadgeUp, IconBlade, IconChevronLeft, IconChevronRight, IconLayoutAlignBottom, IconSearch, IconSettings } from "@tabler/icons-react"
+import { ActionIcon, Avatar, Button, Center, Container, Flex, Group, Header, NavLink, Box, LoadingOverlay, Navbar, Space, Stack, Text, TextInput, Title } from "@mantine/core"
+import { Icon3dCubeSphere, IconAccessible, IconAdjustmentsHorizontal, IconSettingsAutomation, IconAnalyze, IconArrowAutofitUp, IconArrowBadgeDown, IconArrowBadgeUp, IconBlade, IconChevronLeft, IconChevronRight, IconLayoutAlignBottom, IconSearch, IconSettings } from "@tabler/icons-react"
 import { useEffect, useState } from "react"
 import LightDarkButton from "../components/LightDarkButton"
 import { getCustomers, getCustomersProfileCompleteness } from "../redux/customerSlice"
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Logo from '../components/assets/logo.svg';
 
 
 
 const MainAppShell = ({ children }) => {
+
+    const location = useLocation();
+    const [activeNavLink, setActiveNavLink] = useState("")
+
+
+    useEffect(() => {
+        const pathname = location.pathname;
+        setActiveNavLink(pathname);
+    }, [location.pathname]);
+
 
     return (
         <>
@@ -26,25 +36,44 @@ const MainAppShell = ({ children }) => {
             </Header>
             <div style={{ display: 'flex', }}>
                 <span>
-                    <Navbar width={{ base: 50 }} height={500} p="xs" withBorder={false}>
+                    <Navbar height={500} p="xs" withBorder={false}>
                         <Space h={5} />
                         <Stack>
-                            <Link to="/acl">
-                                <ActionIcon variant="subtle" c="cyan">
-                                    <IconSettings size="1rem" />
-                                </ActionIcon>
+                            <Link to="/acl" style={{ textDecoration: "none", fontWeight: "900" }}>
+                                <NavLink
+                                    label="Acl"
+                                    icon={<IconSettingsAutomation size="2rem" stroke={2.0} />}
+                                    active={activeNavLink === "/acl"}
+
+                                >
+                                </NavLink>
                             </Link>
-                            <Link to="/campaign">
-                                <ActionIcon variant="subtle" c='cyan'><Icon3dCubeSphere size="1rem" /></ActionIcon>
+                            <Link to="/campaign" style={{ textDecoration: "none" }}>
+                                <NavLink
+                                    label="Campaign"
+                                    icon={<Icon3dCubeSphere size="2rem" stroke={2} />}
+                                    active={activeNavLink === "/campaign"}
+                                >
+                                </NavLink>
                             </Link>
-                            <Link to="/customers">
-                                <ActionIcon variant="subtle" c='cyan'><IconAccessible size="1rem" /></ActionIcon>
+                            <Link to="/customers" style={{ textDecoration: "none" }}>
+                                <NavLink
+                                    label="Customers"
+                                    icon={<IconAccessible size="2rem" stroke={2} />}
+                                    active={activeNavLink === "/customers"}
+
+                                >
+                                </NavLink>
                             </Link>
-                            <Link to="/">
-                                <ActionIcon variant="subtle" c='cyan'><IconLayoutAlignBottom size="1rem" /></ActionIcon>
+                            <Link to="/" style={{ textDecoration: "none" }}>
+                                <NavLink
+                                    label="Dashboard"
+                                    icon={<IconAnalyze size="2rem" stroke={2} />}
+                                    active={activeNavLink === "/"}
+
+                                >
+                                </NavLink>
                             </Link>
-                            <ActionIcon variant="subtle" c='cyan'><IconAnalyze size="1rem" /></ActionIcon>
-                            <ActionIcon variant="gradient" gradient={{ from: 'black', to: 'indigo' }}><IconArrowBadgeDown size="1rem" /></ActionIcon>
                         </Stack>
                     </Navbar>
                 </span>
--- boilerplate/src/components/PrivateRoute.jsx
+++ boilerplate/src/components/PrivateRoute.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate , Route } from "react-router-dom";
+import { Navigate, Route } from "react-router-dom";
 import { RootState } from "../redux/store";
 
 
 export default function PrivateRoute({ path, ...props }) {
-  console.log(path,props);
+  // console.log(path,props);
   const { isLoggedIn } = useSelector((state) => state.auth);
   if (isLoggedIn) {
     return <Route path={path} {...props} />;
   } else {
-    return <Navigate  to="/login" />;
+    return <Navigate to="/login" />;
   }
 }
