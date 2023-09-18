15ec28d1f4621fa8a0b8753055e4de4dbacc6502
Bryce <bfernandes.extern@europ-assistance.in>
2023-09-18 10:43:28 +0530

    mod file

--- boilerplate/src/AppShell/UserMenuItems/MyAccount/EditProfile.jsx
+++ boilerplate/src/AppShell/UserMenuItems/MyAccount/EditProfile.jsx
@@ -1,80 +1,25 @@
-import React from 'react';
-import {
-    Box, Button, Card, Divider, Stack, Title, TextInput, Select, Group,
-} from '@mantine/core';
-import { useForm } from '@mantine/form';
-import { showNotification } from "@mantine/notifications";
+import { Box, Card, Grid } from '@mantine/core';
+import React, { useState } from 'react';
+import ChangePassword from './ChangePassword';
+import PasswordConstraints from './PasswordConstraints';
 
-const EditProfile = () => {
-    const form = useForm({
-        initialValues: {
-            name: '',
-            address: '',
-            pincode: '',
-            state: '',
-            city: ''
-        },
-    });
-
-    const handleFormSubmit = (values) => {
-        console.log(values);
-        showNotification({
-            type: 'default',
-            title: `Profile updated successfully!`,
-            message: `Your profile details have been updated.`,
-        });
-    };
+const Security = () => {
+    const [password, setPassword] = useState('');
 
     return (
         <Box>
-            <Card shadow='md' p={30}>
-                <Stack>
-                    <Title mb={10} size={'lg'}>Edit Profile</Title>
-                    <Divider />
-                    <form onSubmit={form.onSubmit(handleFormSubmit)}>
-                        <Stack mt={30}>
-                            <TextInput
-                                label="Name"
-                                placeholder="Enter your name"
-                                {...form.getInputProps('name')}
-                            />
-
-                            <TextInput
-                                label="Address"
-                                placeholder="Enter your address"
-                                {...form.getInputProps('address')}
-                            />
-
-                            <Group>
-                                <TextInput
-                                    label="Pincode"
-                                    placeholder="Enter your pincode"
-                                    {...form.getInputProps('pincode')}
-                                    inputMode='numeric'
-                                />
-
-                                <Select
-                                    placeholder="Select your state"
-                                    label="State"
-                                    data={[]}
-                                    {...form.getInputProps('state')}
-                                />
-
-                                <Select
-                                    placeholder="Select your city"
-                                    label="City"
-                                    data={[]}
-                                    {...form.getInputProps('city')}
-                                />
-                            </Group>
-
-                            <Button w={'100%'} type="submit">Update Profile</Button>
-                        </Stack>
-                    </form>
-                </Stack>
+            <Card>
+                <Grid grow>
+                    <Grid.Col span={3}>
+                        <ChangePassword onPasswordChange={setPassword} />
+                    </Grid.Col>
+                    <Grid.Col span={2}>
+                        <PasswordConstraints password={password} />
+                    </Grid.Col>
+                </Grid>
             </Card>
         </Box>
-    );
-};
+    )
+}
 
-export default EditProfile;
+export default Security;
