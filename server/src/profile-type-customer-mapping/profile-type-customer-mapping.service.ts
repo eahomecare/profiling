27ce916456d00f8b0d80176b85b06ca7c684961f
saurabh241930 <sp241930@gmail.com>
2023-10-05 10:04:43 -0700

    Revert "all chnages"
    This reverts commit c70aaec7b57de4ab6429a0edaea9b37f5c48ea14.

--- server/src/profile-type-customer-mapping/profile-type-customer-mapping.service.ts
+++ server/src/profile-type-customer-mapping/profile-type-customer-mapping.service.ts
@@ -17,9 +17,9 @@ export class ProfileTypeCustomerMappingService {
         return this.prisma.profileTypeCustomerMapping.findMany();
     }
 
-    // async findOne(id: string): Promise<ProfileTypeCustomerMapping | null> {
-    //     return this.prisma.profileTypeCustomerMapping.findUnique({ where: { id } });
-    // }
+    async findOne(id: string): Promise<ProfileTypeCustomerMapping | null> {
+        return this.prisma.profileTypeCustomerMapping.findUnique({ where: { id } });
+    }
 
     async update(id: string, data: ProfileTypeCustomerMapping): Promise<ProfileTypeCustomerMapping> {
         return this.prisma.profileTypeCustomerMapping.update({ where: { id }, data });
@@ -37,118 +37,6 @@ export class ProfileTypeCustomerMappingService {
         }));
 
         return groupedCountsArray;
-    }
-
-    async getGroupedCountsByCustomerGender(): Promise<{ gender: string; count: number }[]> {
-        const profileTypeCustomerMappings = await this.prisma.profileTypeCustomerMapping.findMany({
-            include: {
-                customer: {
-                    include: {
-                        personal_details: true,
-                    },
-                },
-            },
-        });
-
-        const groupedCounts = _.groupBy(profileTypeCustomerMappings, (mapping) => mapping.customer.personal_details?.gender || 'Unknown');
-        const groupedCountsArray = _.map(groupedCounts, (group, gender) => ({
-            gender,
-            count: group.length,
-        }));
-
-        return groupedCountsArray;
-    }
-
-    async getGroupedCountsByCustomerSource(): Promise<{ source: string; count: number }[]> {
-        const profileTypeCustomerMappings = await this.prisma.profileTypeCustomerMapping.findMany({ include: { customer: true } });
-
-        const groupedCounts = _.groupBy(profileTypeCustomerMappings, (mapping) => mapping.customer.source || 'Unknown');
-        const groupedCountsArray = _.map(groupedCounts, (group, source) => ({
-            source,
-            count: group.length,
-        }));
-
-        return groupedCountsArray;
-    }
-
-
-
-
-
-    async getGroupedCountsByCustomerAgeRange(): Promise<{ ageRange: string; count: number }[]> {
-        const currentDate = new Date();
-        const ageRanges: { min: number; max: number; label: string }[] = [
-            { min: 0, max: 20, label: '0-20' },
-            { min: 21, max: 25, label: '21-25' },
-            { min: 26, max: 30, label: '26-30' },
-            { min: 30, max: 35, label: '30-35' },
-            { min: 36, max: 40, label: '36-40' },
-            { min: 46, max: 50, label: '46-50' },
-        ];
-
-        const groupedCounts: Record<string, number> = {};
-
-        for (const range of ageRanges) {
-            const minDOB = new Date(
-                currentDate.getFullYear() - range.max,
-                currentDate.getMonth(),
-                currentDate.getDate()
-            );
-            const maxDOB = new Date(
-                currentDate.getFullYear() - range.min,
-                currentDate.getMonth(),
-                currentDate.getDate()
-            );
-
-            const formatDate = (date: Date) => {
-                return date.toISOString();
-            };
-
-            const customers = await this.prisma.profileTypeCustomerMapping.findMany({
-                where: {
-                    customer: {
-                        personal_details: {
-                            date_of_birth: {
-                                gte: formatDate(minDOB),
-                                lte: formatDate(maxDOB),
-                            },
-                        },
-                    },
-                },
-            });
-
-            groupedCounts[range.label] = customers.length;
-        }
-
-        const groupedCountsArray = Object.entries(groupedCounts).map(([ageRange, count]) => ({
-            ageRange,
-            count,
-        }));
-
-        return groupedCountsArray;
-    }
-
-    async getCustomersByGenderForProfileTypeCustomerMappings(gender: string) {
-        try {
-            const customers = await this.prisma.profileTypeCustomerMapping.findMany({
-                where: {
-                    customer: {
-                        personal_details: {
-                            gender: {
-                                equals: gender,
-                            },
-                        },
-                    },
-                },
-            });
-
-            return customers;
-        } catch (error) {
-            console.error('Error fetching customers by gender:', error);
-            throw error;
-        } finally {
-            await this.prisma.$disconnect();
-        }
     }
 
     async getCustomersByAgeRangeForProfileTypeCustomerMappings(ageRange: string) {
@@ -193,5 +81,29 @@ export class ProfileTypeCustomerMappingService {
             await this.prisma.$disconnect();
         }
     }
+
+    async getCustomersByGenderForProfileTypeCustomerMappings(gender: string) {
+        try {
+            const customers = await this.prisma.profileTypeCustomerMapping.findMany({
+                where: {
+                    customer: {
+                        personal_details: {
+                            gender: {
+                                equals: gender,
+                            },
+                        },
+                    },
+                },
+            });
+
+            return customers;
+        } catch (error) {
+            console.error('Error fetching customers by gender:', error);
+            throw error;
+        } finally {
+            await this.prisma.$disconnect();
+        }
+    }
+
 
 }
