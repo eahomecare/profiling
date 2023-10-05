72007ce9479978941b4d466566d97050f11bdecb
Bryce <bfernandes.extern@europ-assistance.in>
2023-10-05 21:10:16 +0530

    fixed agentQuestion and minor stylings

--- boilerplate/src/pages/Acl/RolesVsPermissions.jsx
+++ boilerplate/src/pages/Acl/RolesVsPermissions.jsx
@@ -36,7 +36,7 @@ const RolesVsPermissions = ({ initialData, title }) => {
 
   return (
     <>
-      <Title style={{ padding: "10px" }} order={4} color="blue.5">
+      <Title style={{ padding: "10px" }} order={4} color="#5c00f2">
         {title}
       </Title>
       <StyledTable
