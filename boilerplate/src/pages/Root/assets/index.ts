edc20369aa2172d87758e7bde2f372bb4fa38055
Bryce <bfernandes.extern@europ-assistance.in>
2023-10-10 11:56:12 +0530

    Added pic for profile types

--- boilerplate/src/pages/Root/assets/index.ts
+++ boilerplate/src/pages/Root/assets/index.ts
@@ -3,10 +3,16 @@ import FitnessFreak from "./Fitness Freak.png";
 import Foodie from "./Foodie.png";
 import SportsFan from "./Sports Fan.png";
 import Techie from "./Techie.png";
+import AutoLover from "./Auto Lover.png";
+import Musicophile from "./Musicophile.png";
+import GadgetsFreaks from "./Gadgets Freaks.png";
 import AddProfile from "./AddProfile.png";
 
 export default {
-  "Avid Traveler": AvidTraveler,
+  "Avid Traveller": AvidTraveler,
+  "Auto Lover": AutoLover,
+  Musicophile: Musicophile,
+  "Gadgets Freaks": GadgetsFreaks,
   "Fitness Freak": FitnessFreak,
   Foodie: Foodie,
   "Sports Fan": SportsFan,
