8b6d9dd849cd9513368043bb125186f6f05bb95b
Bryce <bfernandes.extern@europ-assistance.in>
2023-10-10 10:59:30 +0530

    age response change for demographics profile type

--- server/src/profile-type-customer-mapping/profile-type-customer-mapping.controller.ts
+++ server/src/profile-type-customer-mapping/profile-type-customer-mapping.controller.ts
@@ -1,59 +1,92 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { ProfileTypeCustomerMappingService } from './profile-type-customer-mapping.service';
 import { ProfileTypeCustomerMapping } from '@prisma/client';
 
 @Controller('profile-type-customer-mapping')
 export class ProfileTypeCustomerMappingController {
-    constructor(private readonly profileTypeCustomerMappingService: ProfileTypeCustomerMappingService) { }
+  constructor(
+    private readonly profileTypeCustomerMappingService: ProfileTypeCustomerMappingService,
+  ) {}
 
-    @Post()
-    create(@Body() data: ProfileTypeCustomerMapping) {
-        return this.profileTypeCustomerMappingService.create(data);
-    }
+  @Post()
+  create(
+    @Body() data: ProfileTypeCustomerMapping,
+  ) {
+    return this.profileTypeCustomerMappingService.create(
+      data,
+    );
+  }
 
-    @Get()
-    findAll() {
-        return this.profileTypeCustomerMappingService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.profileTypeCustomerMappingService.findAll();
+  }
 
-    // @Get(':id')
-    // findOne(@Param('id') id: string) {
-    //     return this.profileTypeCustomerMappingService.findOne(id);
-    // }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //     return this.profileTypeCustomerMappingService.findOne(id);
+  // }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() data: ProfileTypeCustomerMapping) {
-        return this.profileTypeCustomerMappingService.update(id, data);
-    }
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() data: ProfileTypeCustomerMapping,
+  ) {
+    return this.profileTypeCustomerMappingService.update(
+      id,
+      data,
+    );
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.profileTypeCustomerMappingService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.profileTypeCustomerMappingService.remove(
+      id,
+    );
+  }
 
-    @Get('grouped-counts-by-profile-type-name')
-    async getGroupedCountsByProfileTypeName() {
-        return this.profileTypeCustomerMappingService.getGroupedCountsByCustomerAgeRange();
-    }
+  @Get('grouped-counts-by-profile-type-name')
+  async getGroupedCountsByProfileTypeName() {
+    return this.profileTypeCustomerMappingService.getGroupedCountsByCustomerAgeRange();
+  }
 
-    @Get('by-age-range/:ageRange')
-    async getCustomersByAgeRangeForProfileTypeCustomerMappings(@Param('ageRange') ageRange: string) {
-        return this.profileTypeCustomerMappingService.getCustomersByAgeRangeForProfileTypeCustomerMappings(ageRange);
-    }
+  @Get('by-age-range/:ageRange')
+  async getCustomersByAgeRangeForProfileTypeCustomerMappings(
+    @Param('ageRange') ageRange: string,
+  ) {
+    return this.profileTypeCustomerMappingService.getCustomersByAgeRangeForProfileTypeCustomerMappings(
+      ageRange,
+    );
+  }
 
-    @Get('by-gender/:gender')
-    async getCustomersByGenderForProfileTypeCustomerMappings(@Param('gender') gender: string) {
-        return this.profileTypeCustomerMappingService.getCustomersByGenderForProfileTypeCustomerMappings(gender);
-    }
+  @Get('by-gender/:gender')
+  async getCustomersByGenderForProfileTypeCustomerMappings(
+    @Param('gender') gender: string,
+  ) {
+    return this.profileTypeCustomerMappingService.getCustomersByGenderForProfileTypeCustomerMappings(
+      gender,
+    );
+  }
 
-    @Post('groupByAll')
-    async getGrouped(@Body() data: any) {
-        return {
-            "profiles": await this.profileTypeCustomerMappingService.getGroupedCountsByProfileTypeName(),
-            "gender": await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerGender(),
-            "age": await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerAgeRange(),
-            "source": await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerSource()
-
-        }
-    }
+  @Post('groupByAll')
+  async getGrouped(@Body() data: any) {
+    return {
+      profiles:
+        await this.profileTypeCustomerMappingService.getGroupedCountsByProfileTypeName(),
+      gender:
+        await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerGender(),
+      ageRange:
+        await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerAgeRange(),
+      source:
+        await this.profileTypeCustomerMappingService.getGroupedCountsByCustomerSource(),
+    };
+  }
 }
