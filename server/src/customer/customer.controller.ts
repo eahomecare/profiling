d074ec0c70137edf412fe766bb8b4542ccc178bd
Bryce <bfernandes.extern@europ-assistance.in>
2023-10-11 18:22:23 +0530

    V2 is working and fixed customer add from homecare

--- server/src/customer/customer.controller.ts
+++ server/src/customer/customer.controller.ts
@@ -1,14 +1,14 @@
 import {
-    Body,
-    Controller,
-    Get,
-    HttpCode,
-    HttpStatus,
-    Param,
-    Patch,
-    Post,
-    Response,
-    UseGuards,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Response,
+  UseGuards,
 } from '@nestjs/common';
 import { JwtGuard } from '../auth/guard';
 import { CustomerService } from './customer.service';
@@ -18,104 +18,127 @@ import { CreateCustomerHomecarePayload } from './types';
 // @UseGuards(JwtGuard)
 @Controller('customers')
 export class CustomerController {
-    constructor(
-        private customerService: CustomerService,
-    ) { }
+  constructor(
+    private customerService: CustomerService,
+  ) {}
 
-    @HttpCode(HttpStatus.CREATED)
-    @Post()
-    addCustomerDetails(@Body() dto: any) {
-        return this.customerService.addCustomerDetails(
-            dto,
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  addCustomerDetails(@Body() dto: any) {
+    return this.customerService.addCustomerDetails(
+      dto,
+    );
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  patchCustomerDetails(
+    @Body() dto: any,
+    @Param('id') customerId: string,
+  ) {
+    return this.customerService.patchCustomerDetails(
+      customerId,
+      dto,
+    );
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  getCustomerInfo(
+    @Param('id') customerId: string,
+  ) {
+    console.log('customerId:', customerId);
+    return this.customerService.fetchCustomerInfo(
+      customerId,
+    );
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  getCustomerDetails() {
+    return this.customerService.fetchCustomerDetails();
+  }
+
+  @Post('search_customers_by_attr')
+  async searchCustomers(
+    @Body('information_type')
+    informationType: string,
+    @Body('category') category: string,
+    @Body('value') value: string,
+  ): Promise<Customer[]> {
+    if (
+      informationType === 'personal_information'
+    ) {
+      if (category === 'age') {
+        return this.customerService.getCustomersByAgeRange(
+          value,
         );
-    }
-
-    @HttpCode(HttpStatus.OK)
-    @Patch(':id')
-    patchCustomerDetails(
-        @Body() dto: any,
-        @Param('id') customerId: string,
-    ) {
-        return this.customerService.patchCustomerDetails(
-            customerId,
-            dto,
+      } else if (category === 'gender') {
+        return this.customerService.getCustomersByGender(
+          value,
         );
+      }
+    } else if (informationType === 'interests') {
+      return this.customerService.getCustomersByKeyword(
+        value,
+      );
     }
 
-    @HttpCode(HttpStatus.OK)
-    @Get(':id')
-    getCustomerInfo(
-        @Param('id') customerId: string,
-    ) {
-        console.log('customerId:', customerId);
-        return this.customerService.fetchCustomerInfo(
-            customerId,
+    return [];
+  }
+
+  @Get('/count/monthly')
+  getMonthlyCounts() {
+    return this.customerService.countCustomersByMonth();
+  }
+
+  @Post('/add/customer/generic')
+  @HttpCode(HttpStatus.CREATED)
+  async addCustomer(
+    @Body('customerInput')
+    customerInput: Prisma.CustomerCreateInput,
+    @Body('personalDetailsInput')
+    personalDetailsInput: Prisma.Personal_DetailsCreateInput,
+  ) {
+    try {
+      const createdCustomer =
+        await this.customerService.addCustomer_Homecare(
+          customerInput,
+          personalDetailsInput,
         );
+      return {
+        message: 'Customer created successfully',
+        customer: createdCustomer,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: 'Failed to create customer',
+        error,
+      };
     }
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Get()
-    getCustomerDetails() {
-        return this.customerService.fetchCustomerDetails();
+  @Post('/add/customer/homecare')
+  @HttpCode(HttpStatus.CREATED)
+  async addCustomerHM(
+    @Body() data: CreateCustomerHomecarePayload,
+  ) {
+    try {
+      const createdCustomer =
+        await this.customerService.addCustomerHomecare(
+          data,
+        );
+      return {
+        message: 'Customer created successfully',
+        customer: createdCustomer,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        message: 'Failed to create customer',
+        error,
+      };
     }
-
-    @Post("search_customers_by_attr")
-    async searchCustomers(
-        @Body('information_type') informationType: string,
-        @Body('category') category: string,
-        @Body('value') value: string,
-    ): Promise<Customer[]> {
-        if (
-            informationType === 'personal_information'
-        ) {
-            if (category === 'age') {
-                return this.customerService.getCustomersByAgeRange(
-                    value,
-                );
-            } else if (category === 'gender') {
-                return this.customerService.getCustomersByGender(
-                    value,
-                );
-            }
-
-        } else if (informationType === "interests") {
-            return this.customerService.getCustomersByKeyword(value)
-        }
-
-        return []
-    }
-
-    @Get('/count/monthly')
-    getMonthlyCounts() {
-        return this.customerService.countCustomersByMonth();
-    }
-
-    @Post('/add/customer/generic')
-    @HttpCode(HttpStatus.CREATED)
-    async addCustomer(
-        @Body('customerInput') customerInput: Prisma.CustomerCreateInput,
-        @Body('personalDetailsInput') personalDetailsInput: Prisma.Personal_DetailsCreateInput
-    ) {
-        try {
-            const createdCustomer = await this.customerService.addCustomer_Homecare(customerInput, personalDetailsInput);
-            return { message: 'Customer created successfully', customer: createdCustomer };
-        } catch (error) {
-            console.log(error)
-            return { message: 'Failed to create customer', error };
-        }
-    }
-
-    @Post('/add/customer/homecare')
-    @HttpCode(HttpStatus.CREATED)
-    async addCustomerHM(
-        @Body() data: CreateCustomerHomecarePayload,
-    ) {
-        try {
-            const createdCustomer = await this.customerService.addCustomerHomecare(data);
-            return { message: 'Customer created successfully', customer: createdCustomer };
-        } catch (error) {
-            console.log(error)
-            return { message: 'Failed to create customer', error };
-        }
-    }
+  }
 }
