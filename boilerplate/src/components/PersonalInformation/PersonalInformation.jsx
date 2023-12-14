144665c6d97be7dc299eb3407ce6f46876e2c344
saurabh241930 <sp241930@gmail.com>
2023-12-14 13:22:15 +0530

    Revert "added card in customer dashboard"
    This reverts commit 1bdd4267e83080dfbb5108feb220de3de35c8e3b.

--- boilerplate/src/components/PersonalInformation/PersonalInformation.jsx
+++ boilerplate/src/components/PersonalInformation/PersonalInformation.jsx
@@ -37,13 +37,112 @@ const PersonalInformation = ({ selectedCustomer }) => {
     };
   }, [memoizedSelectedCustomer]);
 
+  const profileCompletion = (percentage) => {
+    return (
+      <div>
+        <Center>
+          <RingProgress
+            mt={50}
+            size={80}
+            thickness={5}
+            sections={[
+              {
+                value: percentage,
+                color:
+                  percentage > 75
+                    ? "#1D9B25"
+                    : percentage > 60
+                      ? "#CFA400"
+                      : "#D85972",
+              },
+            ]}
+            label={
+              <Text color="" weight={20} align="center" size="xs">
+                {percentage}%
+              </Text>
+            }
+          />
+        </Center>
+      </div>
+    );
+  };
 
   if (profile) {
     return (
       <>
-        <Container>
-
-          <div>
+        <Container mt={-80}>
+          <Group grow position="apart">
+            <Text mt={70} fw={500}>
+              Basic Details
+            </Text>
+            <Space h={40} />
+            <div>
+              <Space h={40} />
+              {profileCompletion(profile_completion)}
+            </div>
+          </Group>
+          <div style={{ marginTop: "-30px" }}>
+            {/* <InfoTag title={'Picture'} subject={<Avatar radius={'xl'} size='xl' />} */}
+            <Grid gutter="xl" pt={"sm"}>
+              <Grid.Col span={4}>
+                <InfoTag
+                  title={"First Name"}
+                  subject={profile.full_name.split(" ")[0] && profile.full_name.split(" ")[0]}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <InfoTag
+                  title={"Last Name"}
+                  subject={profile.full_name.split(" ")[1] && profile.full_name.split(" ")[1]}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <InfoTag title={"Gender"} subject={profile.gender} />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <InfoTag title={"Date of birth"} subject={profile.dob} />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <InfoTag title={"Age"} subject={profile.age} />
+              </Grid.Col>
+            </Grid>
+            <Grid gutter="xl">
+              <Grid.Col span={4}>
+                <InfoTag
+                  title={"Marital Status"}
+                  subject={profile.marital_status}
+                />
+              </Grid.Col>
+              <Grid.Col span={4}>
+                <InfoTag title={"Source"} subject={source} />
+              </Grid.Col>
+            </Grid>
+            <Divider my="md" size={"xs"} color={"#4E70EA"} />
+            <Text fw={500}>Contact Details</Text>
+            <Grid gutter="xl">
+              <Grid.Col span={6}>
+                <InfoTag
+                  title={"Email Address"}
+                  subject={customerDetails.email}
+                />
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <InfoTag
+                  title={"Alternative Email"}
+                  subject={profile.alternate_email}
+                />
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <InfoTag title={"Mobile Number"} subject={profile.phone_number} />
+              </Grid.Col>
+              <Grid.Col span={6}>
+                <InfoTag
+                  title={"Alternative Mobile Number"}
+                  subject={profile.alternate_phone_number}
+                />
+              </Grid.Col>
+            </Grid>
+            <Divider my="md" size={"xs"} color={"#4E70EA"} />
             <Text fw={500}>Address</Text>
             <Grid gutter="xl">
               <Grid.Col span={6}>
