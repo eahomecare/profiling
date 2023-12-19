b43d78b648a3bd6e9f42060521a80540214e94d7
saurabh241930 <sp241930@gmail.com>
2023-12-19 18:25:16 +0530

    profile types changes

--- boilerplate/src/redux/profileTypesSlice.js
+++ boilerplate/src/redux/profileTypesSlice.js
@@ -3,16 +3,51 @@ import axios from "axios";
 
 const initialState = {
   data: [],
+  profileTypes: [],
+  profileMappings: [],  // New state for profile mappings
   status: "idle",
+  profileTypesStatus: "idle",
+  profileMappingsStatus: "idle",  // New status for profile mappings
   error: null,
 };
 
+// Async thunk for fetching customer data
 export const fetchData = createAsyncThunk(
   "profiling/fetchData",
   async (customerId) => {
     try {
       const response = await axios.get(
         `${process.env.REACT_APP_API_URL}/profile-type-customer-mapping/customer-mapping/${customerId}`,
+      );
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  },
+);
+
+// Async thunk for fetching profile types
+export const fetchProfileTypes = createAsyncThunk(
+  "profiling/fetchProfileTypes",
+  async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/profile-types`,
+      );
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  },
+);
+
+// Async thunk for fetching profile mappings by profile ID
+export const fetchProfileMappings = createAsyncThunk(
+  "profiling/fetchProfileMappings",
+  async (profileId) => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/profile-type-customer-mapping/profile-customer-mappings-by-profile/${profileId}`,
       );
       return response.data;
     } catch (error) {
@@ -40,6 +75,32 @@ const profilingSlice = createSlice({
         state.error =
           action.error.message ||
           "Failed to fetch profile types for the customer";
+      })
+      .addCase(fetchProfileTypes.pending, (state) => {
+        state.profileTypesStatus = "loading";
+        state.error = null;
+      })
+      .addCase(fetchProfileTypes.fulfilled, (state, action) => {
+        state.profileTypesStatus = "succeeded";
+        state.profileTypes = action.payload;
+      })
+      .addCase(fetchProfileTypes.rejected, (state, action) => {
+        state.profileTypesStatus = "failed";
+        state.error =
+          action.error.message || "Failed to fetch profile types";
+      })
+      .addCase(fetchProfileMappings.pending, (state) => {
+        state.profileMappingsStatus = "loading";
+        state.error = null;
+      })
+      .addCase(fetchProfileMappings.fulfilled, (state, action) => {
+        state.profileMappingsStatus = "succeeded";
+        state.profileMappings = action.payload;
+      })
+      .addCase(fetchProfileMappings.rejected, (state, action) => {
+        state.profileMappingsStatus = "failed";
+        state.error =
+          action.error.message || "Failed to fetch profile mappings";
       });
   },
 });
