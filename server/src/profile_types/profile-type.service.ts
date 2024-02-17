33b65989857b6f75788cbb49a87dca5d3a99b4e2
Bryce <bfernandes.extern@europ-assistance.in>
2024-02-17 08:38:18 +0530

    profile types on init

--- server/src/profile_types/profile-type.service.ts
+++ server/src/profile_types/profile-type.service.ts
@@ -10,6 +10,74 @@ import { UpdateProfileTypeDto } from './dto/update-profile-type.dto';
 export class ProfileTypesService {
   constructor(private prisma: PrismaService) {}
 
+  async onModuleInit() {
+    await this.initProfileTypes();
+  }
+
+  private async initProfileTypes() {
+    const count =
+      await this.prisma.profileType.count();
+    if (count === 0) {
+      const initialProfileTypes: CreateProfileTypeDto[] =
+        [
+          {
+            name: 'food',
+            category: 'food',
+            srcUrl: 'test',
+            description: null,
+          },
+          {
+            name: 'sports',
+            category: 'sports',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'travel',
+            category: 'travel',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'music',
+            category: 'music',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'fitness',
+            category: 'fitness',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'automobile',
+            category: 'automobile',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'gadget',
+            category: 'gadget',
+            srcUrl: null,
+            description: null,
+          },
+          {
+            name: 'technology',
+            category: 'technology',
+            srcUrl: null,
+            description: null,
+          },
+        ];
+
+      await Promise.all(
+        initialProfileTypes.map((type) =>
+          this.createProfileType(type),
+        ),
+      );
+    }
+  }
+
   async createProfileType(
     data: CreateProfileTypeDto,
   ) {
