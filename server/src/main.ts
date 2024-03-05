48b836a1c8b3a3414e9bcb7a93928154c1bfb4c9
Bryce <bfernandes.extern@europ-assistance.in>
2024-03-05 08:52:34 +0530

    Increase request size limit for profile type srcUrl Update

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(
+    express.urlencoded({
+      limit: '50mb',
+      extended: true,
+    }),
+  );
+
   app.enableCors({
     // origin: ['https://custprofile.europ-assistance.in', 'http://localhost:5000'],
     origin: '*',
