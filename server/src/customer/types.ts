114a258c3c654e48ccb8e76e6c1551ad5b319be9
adm_az_ngadwade@eaholding.onmicrosoft.com <ngawade.extern@europ-assistance.in>
2024-05-22 18:15:25 +0530

    CODE UPDATED

--- server/src/customer/types.ts
+++ server/src/customer/types.ts
@@ -27,3 +27,32 @@ export interface CreateCustomerHomecarePayload {
     createdAt: Date;
     updatedAt: Date;
 }
+export interface CreateCustomerHomecareCrmPayload {
+    personalDetails: {
+        ccode: string;
+        country: string;
+        fname: string;
+        lname: string;
+        gender: string;
+        location: string;
+        email: string;
+        mobile: string;
+        memberBenefitId: string;
+        planId: string;
+        clientId: string;
+        programId: string;
+        regCode: string;
+    };
+    socketId: string;
+    cyberior_customer_id: string;
+    homecare_post_id: string;
+    cyberior_activation_status: string;
+    cyberior_id: string;
+    cyberior_user_id: string;
+    cyberior_activation_date: string;
+    registrationVerificationStatus: string;
+    wp_user_id: number;
+    customer_id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
