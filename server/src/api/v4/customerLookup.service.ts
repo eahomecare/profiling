ca3d2f6896f901373514340ccca121dc1a72b0ac
adm_az_ngadwade@eaholding.onmicrosoft.com <ngawade.extern@europ-assistance.in>
2024-05-22 18:15:25 +0530

    CODE UPDATED

--- server/src/api/v4/customerLookup.service.ts
+++ server/src/api/v4/customerLookup.service.ts
@@ -41,6 +41,35 @@ export class CustomerLookupService {
             id: customerMapping.master_customer_id,
           },
         });
+    } else if(crmName === 'HC_CRM'){
+      const customerMapping =
+        await this.prisma.customerHomecareMappingCrm.findFirst(
+          {
+            where: {
+              wp_user_id:
+                typeof customerCRMId === 'string'
+                  ? parseInt(customerCRMId, 10)
+                  : customerCRMId,
+            },
+          },
+        );
+
+        console.log(customerMapping);
+
+      if (!customerMapping) {
+        throw new NotFoundException(
+          `No mapping found for CRM ID ${customerCRMId} and CRM Name ${crmName}`,
+        );
+      }
+
+      customer =
+        await this.prisma.customer.findUnique({
+          where: {
+            id: customerMapping.master_customer_id,
+          },
+        });
+
+        console.log(customer);
     } else {
       // This part is for other CRM names which will be handled later
       const customerMapping =
